b41bbad72ab350a3deb6c4e9475eed16639481a0
Vlademir <vlademirbagaygay@gmail.com>
2026-03-19 09:09:34 +1300

    initialized API and made env.local

--- Movie-App/src/App.jsx
+++ Movie-App/src/App.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import Search from './Components/Search.jsx'
+import {useState, useEffect} from 'react'
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  useEffect(() => {
+
+  }, []);
+
+
   return (
     <main>
       <div className = "pattern"/>
       <div className = "wrapper">
         <header>
+          <img src = "./hero.png" alt="Hero Banner"/>
           <h1>Find <span className = "text-gradient">Movies</span> You'll Enjoy
           Without the Hassle</h1>
         </header>
 
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <h1 className = "text-white">{searchTerm}</h1>
       </div>
     </main>
       
